c32ee8c475633838ae1306bf604406c69af51ee2
Tauseef Mughal <tauseef.mughal@alfresco.com>
2014-02-17 14:00:40 +0000

    MOBILE-2299: Status bar disappears when displaying modal view on iPhone

---
+++ AlfrescoApp/Utils/Categories/UIViewController+Swizzed.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+Swizzed.h
+//  AlfrescoApp
+//
+//  Created by Tauseef Mughal on 17/02/2014.
+//  Copyright (c) 2014 Alfresco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (Swizzed)
+
+@end
---
+++ AlfrescoApp/Utils/Categories/UIViewController+Swizzed.m
@@ -0,0 +1,45 @@
+//
+//  UIViewController+Swizzed.m
+//  AlfrescoApp
+//
+//  Created by Tauseef Mughal on 17/02/2014.
+//  Copyright (c) 2014 Alfresco. All rights reserved.
+//
+
+#import "UIViewController+Swizzed.h"
+#import <objc/objc-runtime.h>
+
+@implementation UIViewController (Swizzed)
+
+- (void)presentViewController_swizzled:(UIViewController *)viewControllerToPresent animated:(BOOL)flag completion:(void (^)(void))completion
+{
+    [self presentViewController_swizzled:viewControllerToPresent animated:flag completion:completion];
+    
+    if (!IS_IPAD || viewControllerToPresent.modalPresentationStyle == UIModalPresentationFullScreen)
+    {
+        [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleDefault animated:YES];
+    }
+}
+
+- (void)dismissViewControllerAnimated_swizzled:(BOOL)flag completion:(void (^)(void))completion
+{
+    [self dismissViewControllerAnimated_swizzled:flag completion:completion];
+    
+    if (!IS_IPAD || self.modalPresentationStyle == UIModalPresentationFullScreen)
+    {
+        [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent animated:YES];
+    }
+}
+
++ (void)load
+{
+    Method defaultPresentViewControllerMethod = class_getInstanceMethod(self, @selector(presentViewController:animated:completion:));
+    Method swizzedPresentViewControllerMethod = class_getInstanceMethod(self, @selector(presentViewController_swizzled:animated:completion:));
+    method_exchangeImplementations(defaultPresentViewControllerMethod, swizzedPresentViewControllerMethod);
+    
+    Method defaultDismissViewControllerAnimatedMethod = class_getInstanceMethod(self, @selector(dismissViewControllerAnimated:completion:));
+    Method swizzledDismissViewControllerAnimatedMethod = class_getInstanceMethod(self, @selector(dismissViewControllerAnimated_swizzled:completion:));
+    method_exchangeImplementations(defaultDismissViewControllerAnimatedMethod, swizzledDismissViewControllerAnimatedMethod);
+}
+
+@end
