c36cb057124d95384515c01deec299c8ab083230
Mike Hatfield <mike.hatfield@alfresco.com>
2014-04-27 01:33:20 +0100

    MOBILE-2454: UILabel+Insets category to allow easy padding of any UILabel

---
+++ AlfrescoApp/Utils/Categories/UILabel+Insets.h
@@ -0,0 +1,16 @@
+//
+//  UILabel+Insets.h
+//  AlfrescoApp
+//
+//  Created by Mike Hatfield on 24/04/2014.
+//  Copyright (c) 2014 Alfresco. All rights reserved.
+//
+
+@interface UILabel (Insets)
+
+@property (nonatomic, assign) CGFloat insetTop;
+@property (nonatomic, assign) CGFloat insetLeft;
+@property (nonatomic, assign) CGFloat insetBottom;
+@property (nonatomic, assign) CGFloat insetRight;
+
+@end
