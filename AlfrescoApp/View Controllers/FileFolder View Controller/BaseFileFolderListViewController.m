44999abfc284d94cb0508197c241da0bc9758ae9
Gavin Cornwell <gavin.cornwell@alfresco.com>
2014-09-17 11:49:05 +0100

    MOBILE-2991: Fix crash in - [BaseFileFolderListViewController tableView:cellForRowAtIndexPath:], line 128

--- AlfrescoApp/View Controllers/FileFolder View Controller/BaseFileFolderListViewController.m
+++ AlfrescoApp/View Controllers/FileFolder View Controller/BaseFileFolderListViewController.m
@@ -125,10 +125,15 @@
             [[ThumbnailManager sharedManager] retrieveImageForDocument:documentNode renditionType:kRenditionImageDocLib session:self.session completionBlock:^(UIImage *image, NSError *error) {
                 if (image)
                 {
-                    AlfrescoNodeCell *updateCell = (AlfrescoNodeCell *)[tableView cellForRowAtIndexPath:indexPath];
-                    if (updateCell)
+                    // MOBILE-2991, check the tableView and indexPath objects are still valid as there is a chance
+                    // by the time completion block is called the table view could have been unloaded.
+                    if (tableView && indexPath)
                     {
-                        [updateCell.image setImage:image withFade:YES];
+                        AlfrescoNodeCell *updateCell = (AlfrescoNodeCell *)[tableView cellForRowAtIndexPath:indexPath];
+                        if (updateCell)
+                        {
+                            [updateCell.image setImage:image withFade:YES];
+                        }
                     }
                 }
             }];
