8dec91b34a274e14e19d18cb2513aa8ffceaa5fe
Tauseef Mughal <tauseef.mughal@alfresco.com>
2015-03-02 11:39:19 +0000

    MOBILE-3179: Support for Open Mode in Document Picker

---
+++ AlfrescoDocumentPicker/Utilities/Utilities.h
@@ -0,0 +1,16 @@
+//
+//  Utilities.h
+//  AlfrescoApp
+//
+//  Created by Tauseef Mughal on 26/02/2015.
+//  Copyright (c) 2015 Alfresco. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Utilities : NSObject
+
++ (NSString *)filenameWithVersionFromFilename:(NSString *)filename nodeIdentifier:(NSString *)nodeIdentifier;
+
+
+@end
