c9bb8833341d4a8519118ab378a1d8af5db41650
Alexandru Posmangiu <Alexandru.Posmangiu@ness.com>
2016-02-18 17:15:43 +0200

    IOS-489: Enable collection of Screen analytics data using Google analytics - added tracking for all sites screen.

--- AlfrescoApp/View Controllers/Sites View Controller/SitesViewController.m
+++ AlfrescoApp/View Controllers/Sites View Controller/SitesViewController.m
@@ -245,7 +245,10 @@ static CGFloat const kSegmentToSearchControlPadding = 8.0f;
             self.siteFinderContainerView.hidden = NO;
             [self.view bringSubviewToFront:self.siteFinderContainerView];
             
-            [[AnalyticsManager sharedManager] trackScreenWithName:kAnalyticsViewSiteListingSearch];
+            if (self.selectedListType == SiteListTypeSelectionAllSites)
+                [[AnalyticsManager sharedManager] trackScreenWithName:kAnalyticsViewSiteListingAll];
+            else
+                [[AnalyticsManager sharedManager] trackScreenWithName:kAnalyticsViewSiteListingSearch];
             
             break;
         }
