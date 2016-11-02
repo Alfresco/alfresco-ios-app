e1ae9c1f6c03394b274c6daada1bf5dc3358af7f
Alexandru Posmangiu <Alexandru.Posmangiu@ness.com>
2016-11-02 17:23:58 +0200

    IOS-839: App not reseted when deleting the last paid account and failing to authenticate via pin IOS-679: PIN entry screen not cleared on successful TouchID entry - removed duplicate code

--- AlfrescoApp/Model/Managers/SecurityManager.m
+++ AlfrescoApp/Model/Managers/SecurityManager.m
@@ -210,19 +210,7 @@
         
         [SecurityManager resetWithType:ResetTypeEntireApp];
         
-        UIViewController *topController = [UniversalDevice topPresentedViewController];
-        
-        if ([topController isKindOfClass:[UINavigationController class]])
-        {
-            UINavigationController *topNavigationController = (UINavigationController *)topController;
-            PinViewController *pvc = topNavigationController.viewControllers.firstObject;
-            
-            if ([pvc isKindOfClass:[PinViewController class]])
-            {
-                [topController dismissViewControllerAnimated:NO completion:nil];
-            }
-        }
-        
+        [self hideCurrentPinViewScreenWithFlow:PinFlowAny animated:NO completionBlock:nil];
         [self showBlankScreen:NO];
         
         [[PreferenceManager sharedManager] updatePreferenceToValue:@(NO) preferenceIdentifier:kSettingsSecurityUsePasscodeLockIdentifier];
@@ -272,29 +260,20 @@
 
 - (void)showPinScreenAnimated:(BOOL)animated inOwnWindow:(BOOL)ownWindow completionBlock:(void (^)())completionBlock
 {
-    UIViewController *topController = [UniversalDevice topPresentedViewController];
+    PinViewController *pvc = [self currentPinScreen];
     
-    if ([topController isKindOfClass:[UINavigationController class]])
+    if (pvc && [pvc pinFlow] == PinFlowEnter)
     {
-        UINavigationController *topNavigationController = (UINavigationController *)topController;
-        PinViewController *pvc = topNavigationController.viewControllers.firstObject;
-        
-        if ([pvc isKindOfClass:[PinViewController class]])
+        if (completionBlock)
         {
-            if ([pvc pinFlow] == PinFlowEnter)
-            {
-                if (completionBlock)
-                {
-                    completionBlock();
-                }
-                
-                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(kKeyboardInPinScreenAppearanceDelay * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-                    [[NSNotificationCenter defaultCenter] postNotificationName:kShowKeyboardInPinScreenNotification object:nil];
-                });
-                
-                return;
-            }
+            completionBlock();
         }
+        
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(kKeyboardInPinScreenAppearanceDelay * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [[NSNotificationCenter defaultCenter] postNotificationName:kShowKeyboardInPinScreenNotification object:nil];
+        });
+        
+        return;
     }
     
     UINavigationController *navController = [PinViewController pinNavigationViewControllerWithFlow:PinFlowEnter inOwnWindow:ownWindow completionBlock:^(PinFlowCompletionStatus status){
@@ -327,7 +306,7 @@
     }
     else
     {
-        [topController presentViewController:navController animated:animated completion:completionBlock];
+        [[UniversalDevice topPresentedViewController] presentViewController:navController animated:animated completion:completionBlock];
     }
 }
 
@@ -383,13 +362,14 @@
     return currentPinViewController;
 }
 
-- (void)hideCurrentPinViewScreenWithcompletionBlock:(void (^)())completionBlock
+- (void)hideCurrentPinViewScreenWithFlow:(PinFlow)pinFlow animated:(BOOL)animated completionBlock:(void (^)())completionBlock
 {
     PinViewController *pvc = [self currentPinScreen];
+    PinFlow currentPinFlow = [pvc pinFlow];
     
-    if (pvc.navigationController && [pvc pinFlow] == PinFlowEnter)
+    if (pvc.navigationController && (currentPinFlow == pinFlow || currentPinFlow == PinFlowAny))
     {
-        [pvc.navigationController dismissViewControllerAnimated:YES completion:completionBlock];
+        [pvc.navigationController dismissViewControllerAnimated:animated completion:completionBlock];
     }
     else
     {
@@ -405,7 +385,7 @@
     [TouchIDManager evaluatePolicyWithCompletionBlock:^(BOOL success, NSError *authenticationError){
         if (success)
         {
-            [self hideCurrentPinViewScreenWithcompletionBlock:^{
+            [self hideCurrentPinViewScreenWithFlow:PinFlowEnter animated:YES completionBlock:^{
                 [self showBlankScreen:NO];
             }];
             
