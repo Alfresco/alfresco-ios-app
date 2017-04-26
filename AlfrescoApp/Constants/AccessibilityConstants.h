48f83d8ce90fdbaaf59b001c0c2aaee6818eb1fd
Silviu Odobescu <silviu.odobescu@ness.com>
2017-04-26 13:54:36 +0300

    IOS-909: Page object ID's for Homescreen

---
+++ AlfrescoApp/Constants/AccessibilityConstants.h
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (C) 2005-2017 Alfresco Software Limited.
+ *
+ * This file is part of the Alfresco Mobile iOS App.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ ******************************************************************************/
+
+#import <Foundation/Foundation.h>
+
+extern NSString * const kOnboardingVCViewIdentifier;
+extern NSString * const kOnboardingVCLoginButtonIdentifier;
+extern NSString * const kOnboardingVCCloseButtonIdentifier;
+
+extern NSString * const kAccountTypeSelectionVCViewIdentifier;
+extern NSString * const kAccountTypeSelectionVCCloudCellIdentifier;
+extern NSString * const kAccountTypeSelectionVCOnPremiseCellIdentifier;
+
+extern NSString * const kNewAccountVCViewIdentifier;
+extern NSString * const kNewAccountVCCancelButtonIdentifier;
+extern NSString * const kNewAccountVCSaveButtonIdentifier;
+extern NSString * const kNewAccountVCUsernameTextfieldIdentifier;
+extern NSString * const kNewAccountVCPasswordTextfieldIdentifier;
+extern NSString * const kNewAccountVCHostnameTextfieldIdentifier;
+extern NSString * const kNewAccountVCDescriptionTextfieldIdentifier;
+extern NSString * const kNewAccountVCHTTPSSwitchIdentifier;
+extern NSString * const kNewAccountVCPortTextfieldIdentifier;
+extern NSString * const kNewAccountVCServiceTextfieldIdentifier;
+extern NSString * const kNewAccountVCClientCetificateCellIdentifier;
+
+extern NSString * const kAccountVCViewIdentifier;
+extern NSString * const kAccountVCAddAccountButtonIdentifier;
+extern NSString * const KAccountVCTableViewIdentifier;
