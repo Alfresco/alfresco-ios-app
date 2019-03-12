cfc8c6ee14c91ed86a1ff49c8d7ae09607df306b
Silviu Odobescu <silviu.odobescu@ness.com>
2019-03-12 18:00:07 +0200

    IOS-1145: Sync unsuccessful on attempt to refresh Synced content where we have checked out files.

--- AlfrescoApp/Utils/Categories/AlfrescoNode+Sync.m
+++ AlfrescoApp/Utils/Categories/AlfrescoNode+Sync.m
@@ -137,22 +137,6 @@
                     {
                         typeToReturn = SyncActivityTypeUpload;
                     }
-                    else
-                    {
-                        //both modifiedAt dates have the same value - checking for last downloaded date
-                        if(syncNode.lastDownloadedDate)
-                        {
-                            NSComparisonResult downloadCompareResult = [syncNode.lastDownloadedDate compare:self.modifiedAt];
-                            if(downloadCompareResult == NSOrderedAscending)
-                            {
-                                typeToReturn = SyncActivityTypeDownload;
-                            }
-                        }
-                        else
-                        {
-                            typeToReturn = SyncActivityTypeDownload;
-                        }
-                    }
                 }
             }
         }
